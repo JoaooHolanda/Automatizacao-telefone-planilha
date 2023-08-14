100294841e907cf2e309876df386f46b30ca647e
joao holanda <joao.holanda@simco.local>
2023-08-14 10:45:27 -0300

    solving 8 char problem

--- Robo_tel(com fixo).py
+++ Robo_tel(com fixo).py
@@ -12,9 +12,6 @@ def Corretor(numero):
     if re.search(padrao, numero):
         return '0'
     
-    if numero.startswith('55'):
-        return numero[2:]
-    
     if len(numero) > 13:
         return numero
     
@@ -23,7 +20,9 @@ def Corretor(numero):
     
     if len(numero) == 12:
         if str(numero)[:2] == "55" or str(numero)[:2] == str(numero)[2:]:
-            return numero[:2] + '9' + numero[2:]
+            i = numero[2:]
+            return i[:2] + "9" + i[2:]
+            
         else:
             return numero
     
@@ -42,18 +41,24 @@ def Corretor(numero):
         return '85' + numero
     
     if len(numero) == 8:
-        return '859' + numero
+        if(numero[:1] != 8 and numero[:1] != 9):
+            return '85' + numero
+        else:
+            return '859' + numero
     
     if len(numero) <= 7:
         return '0'
     
     if len(numero) == 0:
         return '0'
+    
+    if numero == 0:
+        return '0'
 
 def capturar_dados():
     janela.quit()  # Fecha a janela
 
-# Cria a janela principal
+# # Cria a janela principal
 janela = tk.Tk()
 janela.title("Interface de Entrada de Dados")
 
@@ -64,35 +69,34 @@ entrada_var = tk.StringVar()
 titulo_label = tk.Label(janela, text="Nome do Arquivo")
 titulo_label.pack()
 
-# Cria um rótulo para a descrição
+# # Cria um rótulo para a descrição
 descricao_label = tk.Label(janela, text="Digite abaixo o nome que você deseja:")
 descricao_label.pack()
 
-# Cria um campo de entrada
+# # Cria um campo de entrada
 entrada_entry = tk.Entry(janela, textvariable=entrada_var)
 entrada_entry.pack()
 
-# Cria um botão para capturar os dados
+# # Cria um botão para capturar os dados
 capturar_botao = tk.Button(janela, text="Capturar Dados", command=capturar_dados)
 capturar_botao.pack()
 
 
-# Inicia o loop principal da interface gráfica
+# # Inicia o loop principal da interface gráfica
 janela.mainloop()
-
 
 def exibir_alerta(titulo, mensagem):
     ctypes.windll.user32.MessageBoxW(0, mensagem, titulo, 0)
 
 try:
-    tabela = pd.read_excel('Base_A_Tratar.xlsx')
+    tabela = pd.read_excel('Base_A_Tratar-3.xlsx')
 except FileNotFoundError:
-    exibir_alerta('Arquivo não encontrado', 'Por gentileza despejar na pasta um arquivo com o nome: Mailing a tratar.xlsx')
+     exibir_alerta('Arquivo não encontrado', 'Por gentileza despejar na pasta um arquivo com o nome: Mailing a tratar.xlsx')
 
 linha = 0
 
 while linha < len(tabela):
-    nome = str(tabela.loc[linha, 'CD_PESSOA_FISICA'])
+    nome = tabela.loc[linha, 'CD_PESSOA_FISICA']
 
     try:
         number = int(tabela.loc[linha, 'TEL_CELULAR'])
@@ -102,7 +106,9 @@ while linha < len(tabela):
     tabela.loc[tabela['CD_PESSOA_FISICA'] == nome, 'TEL_CELULAR'] = Corretor(number)
     linha += 1
 
+    
 tabela = tabela[(tabela['TEL_CELULAR'] != '0') & (tabela['TEL_CELULAR'] != 0)]
+
 
 data_atual = datetime.datetime.now().date()
 tabela.to_excel(f"{entrada_var.get()}_tratado.xlsx", index=False)
